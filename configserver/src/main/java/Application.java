133715f4fac523a5d81b701dae895f5623dccc0e
www1350 <www_1350@163.com>
2016-09-14 11:39:17 +0800

    增加config  Server进行版本管理

---
+++ configserver/src/main/java/Application.java
@@ -0,0 +1,16 @@
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by Administrator on 2016/9/14.
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(Application.class).web(true).run(args);
+    }
+
+}
--- feign/src/main/java/com/absurd/controller/UserController.java
+++ feign/src/main/java/com/absurd/controller/UserController.java
@@ -2,19 +2,34 @@ package com.absurd.controller;
 
 import com.absurd.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Administrator on 2016/9/14.
  */
 @RestController
+@RefreshScope
 @RequestMapping("/user")
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Value("${userid}")
+    private Long userid;
+
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     @ResponseBody
     public String getUser(@PathVariable("id") Long id){
       return   userService.getUser(id);
     }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String getUser(){
+        return   userService.getUser(userid);
+    }
+
 }
