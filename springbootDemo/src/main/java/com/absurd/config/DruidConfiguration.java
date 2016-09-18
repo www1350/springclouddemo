036c8c18cc232913e46bda00a39234df3a1fa963
www1350 <www_1350@163.com>
2016-09-18 19:36:14 +0800

    @Add dubbo

--- springbootDemo/src/main/java/com/absurd/config/DruidConfiguration.java
+++ springbootDemo/src/main/java/com/absurd/config/DruidConfiguration.java
@@ -1,11 +1,17 @@
 package com.absurd.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
 import com.alibaba.druid.support.http.WebStatFilter;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
-import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+import java.util.Properties;
 
 /**
  * Created by Administrator on 2016/5/20.
@@ -18,6 +24,87 @@ public class DruidConfiguration {
         return new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
     }
 
+    @Value("${spring.datasource.driver-class-name}")
+    private String driver;
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.max-wait}")
+    private Long maxWaitMillis;
+
+    @Value("${spring.datasource.max-active}")
+    private Integer maxActive;
+
+    @Value("${spring.datasource.test-on-borrow}")
+    private Boolean testOnBorrow;
+
+    @Value("${spring.datasource.testOnReturn}")
+    private Boolean testOnReturn;
+
+    @Value("${spring.datasource.testWhileIdle}")
+    private Boolean testWhileIdle;
+
+    @Value("${spring.datasource.initialSize}")
+    private Integer initialSize;
+
+    @Value("${spring.datasource.minIdle}")
+    private Integer minIdle;
+
+    @Value("${spring.datasource.timeBetweenEvictionRunsMillis}")
+    private Long timeBetweenEvictionRunsMillis;
+
+    @Value("${spring.datasource.minEvictableIdleTimeMillis}")
+    private Long minEvictableIdleTimeMillis;
+
+    @Value("${spring.datasource.validationQuery}")
+    private String validationQuery;
+
+    @Value("${spring.datasource.poolPreparedStatements}")
+    private boolean poolPreparedStatements;
+
+    @Value("${spring.datasource.maxPoolPreparedStatementPerConnectionSize}")
+    private Integer maxPoolPreparedStatementPerConnectionSize;
+
+    @Value("${spring.datasource.filters}")
+    private String filters;
+
+    @Value("${spring.datasource.connectionProperties}")
+    private Properties connectionProperties;
+
+    @Bean
+    public DataSource dataSource() {
+        DruidDataSource druidDataSource = new DruidDataSource();
+        druidDataSource.setDriverClassName(driver);
+        druidDataSource.setUrl(url);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+        druidDataSource.setMaxWait(maxWaitMillis);
+        druidDataSource.setMaxActive(maxActive);
+        druidDataSource.setTestOnBorrow(testOnBorrow);
+        druidDataSource.setInitialSize(initialSize);
+        druidDataSource.setMinIdle(minIdle);
+        druidDataSource.setTestOnReturn(testOnReturn);
+        druidDataSource.setTestWhileIdle(testWhileIdle);
+        druidDataSource.setTimeBetweenConnectErrorMillis(timeBetweenEvictionRunsMillis);
+        druidDataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        druidDataSource.setValidationQuery(validationQuery);
+        druidDataSource.setPoolPreparedStatements(poolPreparedStatements);
+        druidDataSource.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+        druidDataSource.setConnectProperties(connectionProperties);
+        try {
+            druidDataSource.setFilters(filters);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return druidDataSource;
+    }
 
 
     @Bean
